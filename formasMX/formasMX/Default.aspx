cf2de3dbe67049856e3388562250195c7e6f7bcf
Luis Navarro Sanchez <luis_firus2@hotmail.com>
2022-04-08 16:52:28 -0500

    Inicio de Repoitorio

---
+++ formasMX/formasMX/Default.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/master-formasmx.Master" CodeBehind="Default.aspx.vb" Inherits="formasMX._Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <a name="contact" id="contact">
+    <div class="needs-validation" novalidate>
+                    <div class="form-row">
+                        <legend class="header">Contacto</legend>                    
+                                           
+                      <div class="col-md-6 mb-3">
+                        <label for="name">Nombre</label>                        
+                          <asp:TextBox runat="server" ID="name" CssClass="form-control" placeholder="Nombre" required></asp:TextBox>
+                        <div class="invalid-feedback">
+                                Por favor, ingrese su Nombre.
+                    </div>
+                      </div>                   
+                    
+                        <div class="col-md-6 mb-3">
+                            <label for="email">Correo</label>
+                                 <div class="input-group">                         
+                                
+                                <asp:TextBox runat="server" type="email" ID="email" CssClass="form-control" placeholder="Email" aria-describedby="inputGroupPrepend" required></asp:TextBox>
+                            <div class="invalid-feedback">
+                                        Por favor, ingrese su correo eléctronico.
+                            </div>
+                         </div>
+                        </div> 
+                    
+                    
+                    <%--  <div class="col-md-6 mb-3">
+                        <label for="meg">Mensaje</label>                        
+                        <asp:TextBox runat="server" ID="msg" type="text" CssClass="form-control" placeholder="Ingrese su mensaje aquí, le responderemos lo antes posible." TextMode="MultiLine" Rows="3" required></asp:TextBox>
+                          <div class="invalid-feedback">
+                          Por favor ingrese su mensaje.
+                        </div>
+                      </div> --%>                         
+                     </div>  
+                        <div class="text-left">
+                            <asp:Button runat="server" ID="enviar" CssClass="btn btn-outline-secondary" Text="Enviar Mensaje" /> 
+                        </div>       
+                 </div>
+        <br />
+    <%--<div class="modal-footer">--%>
+        <asp:Panel ID="procesoStatusContainer" runat="server" Visible="False" EnableViewState="False">
+            <asp:Label ID="procesoStatus" runat="server" Visible="false"></asp:Label>
+            <asp:Image runat="server" ID="imgEnviado" ImageUrl="~/img/mail2.gif" CssClass="card-img" Visible="false" />
+        </asp:Panel>
+        
+    <%--</div>--%> 
+       <script>
+           // Example starter JavaScript for disabling form submissions if there are invalid fields
+           (function () {
+               'use strict';
+               window.addEventListener('load', function () {
+                   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                   var forms = document.getElementsByClassName('needs-validation');
+                   // Loop over them and prevent submission
+                   var validation = Array.prototype.filter.call(forms, function (form) {
+                       form.addEventListener('submit', function (event) {
+                           if (form.checkValidity() === false) {
+                               event.preventDefault();
+                               event.stopPropagation();
+                           }
+                           form.classList.add('was-validated');
+                       }, false);
+                   });
+               }, false);
+           })();
+            </script>    
+    </a></asp:Content>
---
+++ formasMX/formasMX/Default.aspx.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Net.Mail
+
+
+Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+            'No se hace nada
+        End If
+    End Sub
+
+    Protected Sub enviar_Click(sender As Object, e As EventArgs) Handles enviar.Click
+        Try
+            'Se envia el correo electronico
+            enviaCorreo()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub limpiaControles()
+        Try
+            'msg.Text = String.Empty
+            name.Text = String.Empty
+            'subject.Text = String.Empty
+            email.Text = String.Empty
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub enviaCorreo()
+        Try
+
+            If validaCampos() Then
+                Dim msgMail = New MailMessage()
+                Dim myMessage = New MailMessage()
+                Dim mySmtpClient = New SmtpClient()
+
+                Dim destinatarios() As String = ConfigurationManager.AppSettings.Item("destinatarios").Split(",")
+
+                
+
+                'Dim myCredential = New System.Net.NetworkCredential("infopos@merza.com", "gP&M3RzA2018.")
+                Dim myCredential = New System.Net.NetworkCredential(ConfigurationManager.AppSettings.Item("user"), ConfigurationManager.AppSettings.Item("pass"))
+                myMessage.From = New MailAddress(ConfigurationManager.AppSettings.Item("user"), "Formas MX")
+                ' myMessage.To.Add("lnavarro@merza.com")
+
+                For Each correo In destinatarios 'Se agrega el destinatario administrador de los pedidos
+                    If correo.Trim <> "" Then myMessage.To.Add(correo.Trim)
+                Next
+
+                myMessage.Subject = "¡Ústed tiene un nuevo mensaje recibido desde el sitio Formas MX"
+                myMessage.IsBodyHtml = True
+                'Dim mensajeCorreo As String = "<div align=""center""><img src=""http://www.formas.mx/img/formas_2.jpg"" width=""640"" height=""315""  /><p><h1>¡Nuevo mensaje recibido!</h1></p>" _
+                '                           & "<p>Recibido el día: " & Now.Date.ToString("dd/MM/yyyy") & "</p>" _
+                '                           & "<p>&nbsp;</p>" _
+                '                           & "<p>Hola, <strong>" & name.Text & "</strong> envío un mensaje:<br />" _
+                '                           & "<p><strong>" & msg.Text & "</strong></p>" _
+                '                           & "<p><strong>Email:</strong></p>" _
+                '                           & "<p>" & email.Text & "</p><br />" _
+                '                           & "<br />" _
+                '                           & "<strong>Saludos,</strong><br />" _
+                '                           & "<strong>Equipo Formas MX.</strong><br /></div>"
+
+                Dim mensajeCorreo As String = "<div align=""center""><img src=""http://www.formas.mx/img/formas_2.jpg"" width=""640"" height=""315""  /><p><h1>¡Nuevo mensaje recibido!</h1></p>" _
+                                           & "<p>Recibido el día: " & Now.Date.ToString("dd/MM/yyyy") & "</p>" _
+                                           & "<p>&nbsp;</p>" _
+                                           & "<p>Hola, <strong>" & name.Text & "</strong> envío un mensaje:<br />" _
+                                           & "<p><strong>Email:</strong></p>" _
+                                           & "<p>" & email.Text & "</p><br />" _
+                                           & "<br />" _
+                                           & "<strong>Saludos,</strong><br />" _
+                                           & "<strong>Equipo Formas MX.</strong><br /></div>"
+
+                myMessage.Body = mensajeCorreo
+                'mySmtpClient.Host = "smtp.gmail.com"
+                mySmtpClient.Host = ConfigurationManager.AppSettings.Item("host")
+                mySmtpClient.EnableSsl = ConfigurationManager.AppSettings.Item("ssl")
+                'mySmtpClient.Port = 587
+
+                mySmtpClient.Port = ConfigurationManager.AppSettings.Item("puerto")
+                mySmtpClient.UseDefaultCredentials = False
+                mySmtpClient.Credentials = myCredential
+                mySmtpClient.ServicePoint.MaxIdleTime = 1
+
+                mySmtpClient.Send(myMessage)
+                myMessage.Dispose()
+
+                If Request.Browser.IsMobileDevice Then
+                    procesoStatusContainer.Visible = True
+                    imgEnviado.Visible = True
+                Else
+                    procesoStatusContainer.Visible = True
+                    procesoStatusContainer.CssClass = "alert-success"
+                    procesoStatus.Visible = True
+                    procesoStatus.Text = "¡Mensaje enviado con exito!"
+                End If
+
+                limpiaControles()
+            Else
+                Exit Sub
+            End If
+        Catch ex As Exception
+            procesoStatusContainer.Visible = True
+            procesoStatus.Visible = True
+            procesoStatus.Text = ex.Message
+        End Try
+    End Sub
+
+    Private Function validaCampos() As Boolean
+        Try
+            validaCampos = False
+            'If name.Text = String.Empty OrElse subject.Text = String.Empty OrElse email.Text = String.Empty Or msg.Text = String.Empty Then
+            'If name.Text = String.Empty OrElse email.Text = String.Empty Or msg.Text = String.Empty Then
+            If name.Text = String.Empty OrElse email.Text = String.Empty Then
+                validaCampos = False
+            End If
+            validaCampos = True
+        Catch ex As Exception
+            validaCampos = False
+        End Try
+    End Function
+
+
+End Class
